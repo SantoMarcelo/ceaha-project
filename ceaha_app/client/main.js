715395c0260c91c4454befba921620b68d1b519f
Marcelo <santos.marcelooliveira@outlook.com>
2018-09-17 20:52:54 -0300

    addicionado dados profissionais no html

--- ceaha_app/client/main.js
+++ ceaha_app/client/main.js
@@ -50,5 +50,41 @@ Template.novoParticipante.events({
 })
 
 
+Template.novoParticipante.events({
+  'click #cadastrar'(event, instance){
+    event.preventDefault();
+    var participante = {
+      nome: $('#full-name').val(),
+      nascimento: $('#date-birth').val(),
+      nacionalidade: $('#nacionality').val(),
+      cidade: $('#city').val(),
+      uf: $('#uf').val(),
+      nome_pai: $('#father-name').val(),
+      rg: $('#mother-name').val(),
+      nome_mae: $('#rg').val(),
+      orgao_emissor: $('#orgao-emissor').val(),
+      cpf: $('#cpf').val(),
+      estado_civil: $('#inputEstadoCivil').val(),
+      cep: $('#cep').val(),
+      logradouro: $('#rua').val(),
+      numero: $('#numero').val(),
+      bairro: $('#bairro').val(),
+      complemento: $('#complemento').val(),
+      uf_endereco: $('#uf-endereco').val()
+      
+
+    }
+
+    Meteor.call('inserirParticipante', participante, function(err, res){
+      if (err) {
+          sAlert.error(err.reason)
+          return false;
+      } else {
+          sAlert.success('Participante cadastrado com sucesso.')
+      }
+  })     
+  }
+})
+
 
 
